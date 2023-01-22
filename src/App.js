880c2ee4b8a34188d03a38f475216c2dc44cb101
milliorn <scottmilliorn@gmail.com>
2023-01-21 19:27:45 -0800

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Signin from "./pages/Signin";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
       <AuthContextProvider>
+        <Navbar />
         <Routes>
           <Route path="/signin" element={<Signin />} />
         </Routes>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+
+function Navbar() {
+  const { user, logOut } = UserAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-between bg-gray-200 w-full p-4">
+      <h1 className="text-center text-2xl font-bold">Firebase Google Auth</h1>
+      {user?.displayName ? (
+        <button onClick={handleSignOut}>Logout</button>
+      ) : (
+        <Link to="/signin">Sign in</Link>
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
