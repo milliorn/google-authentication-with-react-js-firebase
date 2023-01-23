ec56ee57409ee3fe37c1f034513ff18955f0b64d
milliorn <scottmilliorn@gmail.com>
2023-01-22 17:38:02 -0800

    Home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 
 function App() {
@@ -10,9 +11,9 @@ function App() {
       <AuthContextProvider>
         <Navbar />
         <Routes>
-          <Route path="/"/>
+          <Route path="/" element={<Home />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/account"/>
+          <Route path="/account" />
         </Routes>
       </AuthContextProvider>
     </div>
